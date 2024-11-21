a8d934fb54254aabba67a2e0889dd2c998b5f65b
Dailson Campos <dcampos.dev@gmail.com>
2024-11-21 20:25:41 -0300

    melhoria no tratamento  de erros

--- request.py
+++ request.py
@@ -2,7 +2,14 @@ import requests
 
 class Request:
     def __init__(self, url, setup_instance):
-        self.response = requests.get(url, headers=setup_instance.get_headers())
+        self.url = url
+        self.headers = setup_instance.get_headers()
 
     def get_response(self):
-        return self.response
+        try:
+            response = requests.get(self.url, headers=self.headers)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao fazer a requisição para {self.url}: {e}")
+            return None
