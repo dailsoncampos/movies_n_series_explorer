f4a6690a5addbaa4175900f3fa45f0d3e9d4d6ab
Dailson Campos <dcampos.dev@gmail.com>
2024-11-21 19:53:25 -0300

    ajuste de parâmetro

--- request.py
+++ request.py
@@ -1,8 +1,8 @@
 import requests
 
 class Request:
-    def __init__(self, search_instance, setup_instance):
-        self.response = requests.get(search_instance.url, headers=setup_instance.get_headers())
+    def __init__(self, url, setup_instance):
+        self.response = requests.get(url, headers=setup_instance.get_headers())
 
     def get_response(self):
         return self.response
