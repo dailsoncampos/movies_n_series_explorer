a790a6ad8b10279df89e6bc95120f049cf6cda42
Dailson Campos <dcampos.dev@gmail.com>
2024-11-21 20:26:48 -0300

    lida com erros de credenciais

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 import yaml
 
 class Setup:
-    def __init__(self):
-        with open('credentials.yml', 'r') as file:
-            credentials = yaml.safe_load(file)
+    def __init__(self, credentials_file='credentials.yml'):
+        self.headers = self.load_credentials(credentials_file)
 
-        self.headers = {
-            "accept": "application/json",
-            "Authorization": "Bearer " + credentials['api_token']
-        }
+    @staticmethod
+    def load_credentials(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                credentials = yaml.safe_load(file)
+                return {
+                    "accept": "application/json",
+                    "Authorization": f"Bearer {credentials['api_token']}"
+                }
+        except FileNotFoundError:
+            raise FileNotFoundError("Arquivo de credenciais não encontrado.")
+        except KeyError:
+            raise KeyError("Token de API não encontrado nas credenciais.")
+        except Exception as e:
+            raise Exception(f"Erro ao carregar as credenciais: {e}")
 
     def get_headers(self):
         return self.headers
