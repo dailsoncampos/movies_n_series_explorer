9dcf8e41d0b7542d957abed1080c0114d7cb530b
Dailson Campos <dcampos.dev@gmail.com>
2024-11-21 20:25:18 -0300

    unificação da lógica para filmes e séries

--- presenter.py
+++ presenter.py
@@ -1,27 +1,8 @@
 class Presenter:
-    def __init__(self, movie_content, tv_series_content):
-        if movie_content.status_code == 200 and tv_series_content.status_code:
-            movie_data = movie_content.json()
-            tv_series_data = tv_series_content.json()
+    @staticmethod
+    def present(data, content_type):
+        items = [{'title': item.get('original_title') or item.get('original_name'), 'overview': item['overview']}
+                 for item in data.get('results', [])]
 
-            print("Lista de filmes")
-            self.movies_list(movie_data)
-
-            print("Lista de séries")
-            self.tv_series_list(tv_series_data)
-
-        else:
-            print(f"Erro {movie_content.status_code}: Não foi possível obter os dados.")
-            print(f"Erro {tv_series_content.status_code}: Não foi possível obter os dados.")
-
-    def movies_list(self, data):
-        movies = [{'title': movie['original_title'], 'overview': movie['overview']} for movie in data['results']]
-
-        for item in movies:
-            print(f"\nTitle: {item['title']}\nOverview: {item['overview']}")
-
-    def tv_series_list(self, data):
-        tv_series = [{'title': movie['original_name'], 'overview': movie['overview']} for movie in data['results']]
-
-        for item in tv_series:
-            print(f"\nTitle: {item['title']}\nOverview: {item['overview']}")
+        for item in items:
+            print(f"\nTítulo ({content_type}): {item['title']}\nResumo: {item['overview']}")
