958fac2bfc0b0e57bb4d6dea98468161a3a266d8
Dailson Campos <dcampos.dev@gmail.com>
2024-11-21 20:23:24 -0300

    melhora a organização e reduz repetições

--- core.py
+++ core.py
@@ -5,19 +5,23 @@ from presenter import Presenter
 
 class Core:
     def __init__(self):
-        pass
+        self.setup_instance = Setup()
+        self.search_instance = Search()
+
+    def fetch_and_present(self, search_method, content_type):
+        url = search_method()
+        response = Request(url, self.setup_instance).get_response()
+
+        if response.status_code == 200:
+            data = response.json()
+            print(f"\nLista de {content_type}s")
+            Presenter.present(data, content_type)
+        else:
+            print(f"Erro {response.status_code}: Não foi possível obter os dados de {content_type}s.")
 
     def call(self):
-        setup_instance = Setup()
-        search_instance = Search()
-
-        movie_url = search_instance.for_movie()
-        movies_response = Request(movie_url, setup_instance).get_response()
-
-        tv_series_url = search_instance.for_tv_series()
-        tv_series_response = Request(tv_series_url, setup_instance).get_response()
-
-        Presenter(movies_response, tv_series_response)
+        self.fetch_and_present(self.search_instance.for_movie, "filme")
+        self.fetch_and_present(self.search_instance.for_tv_series, "série")
 
 core_instance = Core()
 core_instance.call()
