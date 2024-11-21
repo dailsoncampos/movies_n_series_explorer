bafe5c1339d64fdaca0585a654cb737d362dae0e
Dailson Campos <dcampos.dev@gmail.com>
2024-11-21 20:26:19 -0300

    Valida entrada e evita repetição

--- search.py
+++ search.py
@@ -1,9 +1,20 @@
 class Search:
+    BASE_URL = "https://api.themoviedb.org/3/search"
+
     def __init__(self):
-        self.title = input("Pesquisar por: ")
-    
+        self.title = self.get_title()
+
+    def get_title(self):
+        title = input("Pesquisar por: ").strip()
+        if not title:
+            raise ValueError("O título não pode ser vazio!")
+        return title
+
+    def generate_url(self, type_):
+        return f"{self.BASE_URL}/{type_}?query={self.title}"
+
     def for_movie(self):
-        return f"https://api.themoviedb.org/3/search/movie?query={self.title}"
+        return self.generate_url("movie")
 
     def for_tv_series(self):
-        return f"https://api.themoviedb.org/3/search/tv?query={self.title}"
+        return self.generate_url("tv")
